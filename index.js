a7dbaa13f874af9cc8acbf26d1fa18e2f90cc790
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2024-01-04 20:44:56 +0100

    Anzeigen der Order

--- index.js
+++ index.js
@@ -84,30 +84,30 @@ io.on('connection', (socket) => {
 
         
 
-    for (let i = 0; i < order.length; i++) {
-        if (order[i].id === data.id) {
-            index = i;
+        for (let i = 0; i < order.length; i++) {
+            if (order[i].id === data.id) {
+                index = i;
+            }
         }
-    }
-    
-    if (index === -1) {
-        console.log("new player");
-        order.push({ id: data.client_id, position: data.position, current_lap: data.current_lap });
-    } else {
-        order[index].position = data.position;
-        order[index].current_lap = data.current_lap;
-    }
-
-    order.sort((a, b) => {
-        if (a.current_lap === b.current_lap) {
-            return a.position - b.position;
+        
+        if (index === -1) {
+            console.log("new player");
+            order.push({ id: data.client_id, position: data.position, current_lap: data.current_lap });
+        } else {
+            order[index].position = data.position;
+            order[index].current_lap = data.current_lap;
         }
-        return a.current_lap - b.current_lap;
-    });
 
-    io.emit("receive_order", order);
-    }
-    )
+        order.sort((a, b) => {
+            if (a.current_lap === b.current_lap) {
+                return a.position - b.position;
+            }
+            return a.current_lap - b.current_lap;
+        });
+
+        io.emit("receive_order", order);
+        }
+        )
 });
 server.listen(3000, '0.0.0.0', () => {
     console.log('server running at http://35.246.239.15:3000');
